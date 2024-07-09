ee823154d28c6145ace8c510062b1583648e1c09
CohenAvigail <ac8531286@gmail.com>
2024-07-09 23:07:42 +0300

    Improving the code according to requirements

--- client-side/src/components/Examples.jsx
+++ client-side/src/components/Examples.jsx
@@ -1,14 +1,26 @@
-import MyButton from './MyButton';
+import GenericButton from './GenericButton';
 
+
+function func1(){
+    console.log("hello1")
+}
+
+function func2(){
+    console.log("hello2")
+}
+
+function func3(){
+    console.log("hello3")
+}
 export default function Example(){
     return(
         <>
-            <MyButton className="secondary" size="small" label="secondary1"></MyButton>
-            <MyButton className="secondary" disabled={true} size="medium" label="secondary2"></MyButton>
-            <MyButton className="secondary" size="large" label="secondary3"></MyButton>
-            <MyButton className="primary" size="small" label="primary1"></MyButton>
-            <MyButton className="primary" disabled={true} size="medium" label="primary2"></MyButton>
-            <MyButton className="primary" size="large" label="primary3"></MyButton>
+            <GenericButton onClick={func1} className="secondary" size="small" label="secondary1"></GenericButton>
+            <GenericButton onClick={func2} className="secondary" disabled={true} label="secondary2"></GenericButton>
+            <GenericButton onClick={func3} className="secondary" size="large" label="secondary3"></GenericButton>
+            <GenericButton onClick={func1} className="primary" size="small" label="primary1"></GenericButton>
+            <GenericButton onClick={func2} className="primary" disabled={true} label="primary2"></GenericButton>
+            <GenericButton onClick={func3} className="primary" size="large" label="primary3"></GenericButton>
         </>
     )
 }
