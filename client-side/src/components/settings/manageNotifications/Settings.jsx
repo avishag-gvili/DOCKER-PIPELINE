401288b2cb45687cb4b1e4d3e4202d87444b2503
moriya2004 <mor2004@gmail.com>
2024-07-17 16:10:48 +0300

    remove time-zone comp

--- client-side/src/components/settings/manageNotifications/Settings.jsx
+++ client-side/src/components/settings/manageNotifications/Settings.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import EmailFrequency from './EmailFrequency.jsx';
-import TimeZone from './TimeZone.jsx'
 
 const Settings = () => {
   return (
     <div className="manage-notifications">
       <h2>Settings</h2>
       <EmailFrequency></EmailFrequency>
-      <TimeZone></TimeZone>
     </div>
   );
 };
