9b0ce0bca099cc1271999a74e0620fbe40b68250
moriya2004 <mor2004@gmail.com>
2024-07-17 16:18:52 +0300

    add time-zone comp

--- client-side/src/components/settings/manageNotifications/Settings.jsx
+++ client-side/src/components/settings/manageNotifications/Settings.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import EmailFrequency from './EmailFrequency.jsx';
+import TimeZone from './TimeZone.jsx';
 
 const Settings = () => {
   return (
     <div className="manage-notifications">
       <h2>Settings</h2>
       <EmailFrequency></EmailFrequency>
+      <TimeZone></TimeZone>
     </div>
   );
 };
