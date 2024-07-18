4f067fa16e40487c5a4ee845e6d565d52febe655
moriya2004 <mor2004@gmail.com>
2024-07-18 13:26:57 +0300

    update prefernce.model

--- server-side/models/preference.model.js
+++ server-side/models/preference.model.js
@@ -4,7 +4,5 @@ const PreferenceSchema= new mongoose.Schema({
     emailFrequency:{type:String,default:'never',enum:['never','daily', 'weekly','monthly','yearly']},
     sendNotificationTime:{type:Number,default:30,required:true},
     soundVoice:{type:String,default:'×××ª ××¢× ×¢×©×'},
-    timeZone: { type: String, default: 'UTC' },
-    language: { type: String, default: 'en' }
 })
 export default mongoose.model('Preference',PreferenceSchema);
