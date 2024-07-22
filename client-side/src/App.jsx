87d22592ea1690ce6ce91f1cfff00a1271005c86
ayeletBinder <ayeletbinder@gmail.com>
2024-07-22 16:18:36 +0300

    loader component

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './stories/footer/FooterComponent';
 import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
 import './App.scss';
+import Loader from './stories/loader/loader.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <RouterProvider router={router} />
       <Provider store={store}>
         <Footer />
+        <Loader/>
       </Provider>
     </>
   );
